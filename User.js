10df8c2e9fc258f0e991f136983376ecd058bb7a
Urji Abdurahman <urjiabdurehman55@gmail.com>
2026-04-03 20:07:16 +0300

    Urjii Ab Backend

--- User.js
+++ User.js
@@ -4,7 +4,11 @@ const UserSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    phone: { type: String },
+    password: { type: String, required: true },
+    gender: { type: String },
+    age: { type: Number },
+    country: { type: String }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
