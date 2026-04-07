d19bf5348832da038937838e4e9c2f14f5a33327
Urji Abdurahman <urjiabdurehman55@gmail.com>
2026-04-07 15:00:03 +0300

    Urjii Ab Backend

--- order.js
+++ order.js
@@ -4,17 +4,24 @@ const fileSchema = new mongoose.Schema({
     originalName: String,
     mimeType: String,
     size: Number,
-    data: Buffer // Storing file directly in MongoDB via Buffer
+    data: Buffer
 });
 
 const orderSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   service: { type: String, required: true },
   name: { type: String, required: true },
+  jobTitle: { type: String },
+  companyName: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
-  description: { type: String, required: true },
-  files: [fileSchema], // Array of uploaded files
+  businessProblem: { type: String, required: true },
+  hasWebsite: { type: String, required: true },
+  launchDate: { type: Date, required: true },
+  budgetRange: { type: String, required: true },
+  primaryGoal: { type: String, required: true },
+  preferredCommunication: { type: String, required: true },
+  files:[fileSchema],
   status: { type: String, default: 'Pending' }
 }, { timestamps: true });
 
